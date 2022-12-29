0d95f720929b1961c28b689e011dcdf0d8dd2547
Electrolytful <erhanulusal@hotmail.com>
2022-12-29 16:50:06 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [age, setAge] = useState(0);
+
+
+  const increaseAge = () => {
+    setAge(age + 1);
+  }
+
+  const resetAge = () => {
+    setAge(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      {age}
+      <button onClick={increaseAge}>Increase Age</button>
+      <button onClick={resetAge}>Reset Age</button>
     </div>
   );
 }
+
+
 
 export default App;
